a083238cbffd97e87f80a330b8bbaa1c39dd02f9
Manish <2002mybaran@gmail.com>
2022-06-13 06:11:17 +0530

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 
 
 const port = process.env.PORT || 5000
 
+connectDB()
+
 const app = express()
+
 
 // middleware 
 app.use(express.json())
@@ -14,6 +19,7 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 
-app.use(errorHandler)
+app.use(errorHandler) //overright default error handler
+
 app.listen(port,()=>console.log(`server started on port ${port}`))
 
